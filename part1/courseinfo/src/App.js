10a000c0ddb6c33528e1b45128fcf3865e1a9b3f
SilentGamelan <28412081+SilentGamelan@users.noreply.github.com>
2021-07-01 11:02:52 +0100

    Complete ex1.4 - refactor parts to objs

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -17,9 +17,9 @@ const Part = (part)=> {
 const Content = (props)=>{
   return (
     <div>
-      <Part name={props.part1} exercise={props.exercise1} />
-      <Part name={props.part2} exercise={props.exercise2} />
-      <Part name={props.part3} exercise={props.exercise3} />
+      <Part name={props.part1.name} exercise={props.part1.exercises} />
+      <Part name={props.part2.name} exercise={props.part2.exercises} />
+      <Part name={props.part3.name} exercise={props.part3.exercises} />
     </div>
     )
 }
@@ -35,24 +35,26 @@ const Total = (props) => {
 
 const App = ()=> {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises : 10
+  }
+  const part2 = { 
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  
+  const part3 = { 
+    name: 'State of a component',
+    exercises:  14
+  }
 
-  const t = [1,2,3];
-
-
-
-  console.log(t);
-
+  
   return (
     <div>
       <Header headerText={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
-      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />  
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />  
     </div>
   )
 }
