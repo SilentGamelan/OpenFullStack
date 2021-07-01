27650d3f1dc08bce9ba2b1731a2893fea93e1e02
SilentGamelan <28412081+SilentGamelan@users.noreply.github.com>
2021-07-01 14:41:19 +0100

    implement stateful counter with buttons

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,65 +1,22 @@
-import React from 'react'
-
-const Header = (courses)=> {
-  return (
-  <h1>{courses.course.name}</h1>
-  )
-}
-
-const Part = (part)=> {
-  return(
-    <div>
-      {part.name} {part.exercises}
-    </div>
-  )
-}
-
-const Content = (courses)=>{
-  return (
-    <div>
-      <Part name={courses.course.parts[0].name} exercises={courses.course.parts[0].exercises} />
-      <Part name={courses.course.parts[1].name} exercises={courses.course.parts[1].exercises} />
-      <Part name={courses.course.parts[2].name} exercises={courses.course.parts[2].exercises} />
-      </div>
-    )
-}
-
-const Total = (courses) => {
-  return (
-    <p>
-      Number of exercises {courses.course.parts[0].exercises + courses.course.parts[1].exercises + courses.course.parts[2].exercises}
-    </p>
-  )
-}
-
+import React, {useState} from 'react'
 
 const App = ()=> {
-  const course = { 
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises : 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      { 
-        name: 'State of a component',
-        exercises:  14
-      }
-    ]
-  };
-  
-  
+  const [counter, setCounter] = useState(0);
+
+  const incByOne = ()=> setCounter(counter+1);
+  const setToZero= ()=> setCounter(0);
+  const decByOne = ()=> setCounter(counter-1);
+    
+
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+    <div>{counter}</div>
+    <button onClick={decByOne}>-1</button>
+    <button onClick={setToZero}>CLEAR</button>
+    <button onClick={incByOne}>+</button>
+
     </div>
   )
 }
 
-export default App;
+export default App
