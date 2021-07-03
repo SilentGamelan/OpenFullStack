599c56f3935b03b7885dbe9f3908cda5bcfc2bbe
SilentGamelan <28412081+SilentGamelan@users.noreply.github.com>
2021-07-03 12:06:25 +0100

    Implement parameterised event handlers

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,51 +1,26 @@
 import React, {useState} from 'react'
 
-const History = (props) => {
-  if(props.clickHistory.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        button press history: {props.clickHistory.join('')}
-      </div>
-    )
-  }
-}
 
-const Button = ({handleClick, buttonText}) => {
-  return <button onClick={handleClick}>{buttonText}</button>
-}
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.buttonText}</button>
+)
 
 const App = ()=> {
-  const [leftClicks, setLeftClicks] = useState(0);
-  const [rightClicks, setRightClicks] = useState(0);
-  const [clickHistory, setClickHistory] = useState([]);
+  const [value, setValue] = useState(0);
 
-  const handleLeftClick = () => {
-    setClickHistory(clickHistory.concat('L'));
-    setLeftClicks(leftClicks + 1);
-  }
-
-  const handleRightClick = () => {
-    setClickHistory(clickHistory.concat('R'));
-    setRightClicks(rightClicks + 1);
-  }
-
-
+  const setToValue = (newValue) => () => setValue(newValue);
 
   return (
     <div>
-      {leftClicks}
-      <Button handleClick={handleLeftClick} buttonText="Left Button" />
-      <Button handleClick={handleRightClick} buttonText="Right Button" />
-      {rightClicks}
-      <History clickHistory={clickHistory} />
+      <Display value={value} />
+      <Button handleClick={setToValue(1000)} buttonText="1000" />
+      <Button handleClick={setToValue(0)} buttonText="0"/>
+      <Button handleClick={setToValue(value+1)} buttonText="+1" />
     </div>
   )
+
 };
 
 export default App
