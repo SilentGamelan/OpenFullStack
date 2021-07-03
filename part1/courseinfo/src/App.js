a9423f5488161de2019be14d8a24da299ea357ee
SilentGamelan <28412081+SilentGamelan@users.noreply.github.com>
2021-07-03 13:52:23 +0100

    Refactors statistics into components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,83 @@
 import React, {useState} from 'react'
 
 
-const Display = props => <div>{props.value}</div>
+const Button = ({handleClick, buttonText} ) => {
+  return (
+      <button onClick={handleClick}>{buttonText}</button>
+  )
+}
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.buttonText}</button>
-)
+const Statistic = ({value, text}) => {
+  return (
+    <div>{text} {value}</div>
+  )
+}
+
+const Statistics = ({stats}) => {
+  
+  const all = stats.good + stats.neutral + stats.bad
+  if( all === 0) {
+    return (
+      <div>
+        No Feedback Given
+      </div>
+    )
+  } 
+  
+  const average = (stats.good - stats.bad);
+  const positive = (stats.good / all);
+
+  return(
+    <div>
+      <Statistic text="good" value={stats.good} />
+      <Statistic text="neutral" value={stats.neutral} />
+      <Statistic text="bad" value={stats.bad} />
+      <Statistic text="all" value={all} />
+      <Statistic text="average" value={average} />
+      <Statistic text="positive" value={positive} />
+    </div>
+  )
+
+}
 
 const App = ()=> {
-  const [value, setValue] = useState(0);
+  const [goodFeedback, setGood] = useState(0);
+  const [neutralFeedback, setNeutral] = useState(0);
+  const [badFeedback, setBad] = useState(0);
 
-  const setToValue = (newValue) => () => setValue(newValue);
+  const stats = {
+    good: goodFeedback,
+    neutral: neutralFeedback,
+    bad: badFeedback
+  }
+
+  
+  const handleBadClick = () => {
+    setBad(badFeedback + 1);
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutralFeedback + 1);
+  }
+  const handleGoodClick = () => {
+    setGood(goodFeedback + 1);
+  }
+
 
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={setToValue(1000)} buttonText="1000" />
-      <Button handleClick={setToValue(0)} buttonText="0"/>
-      <Button handleClick={setToValue(value+1)} buttonText="+1" />
+      <h1>Give Feedback</h1>
+      <Button handleClick={handleGoodClick} buttonText="Good" />
+      <Button handleClick={handleNeutralClick} buttonText="Neutral"/>
+      <Button handleClick={handleBadClick} buttonText="Bad" />
+
+      <h1>Statistics</h1>
+      <Statistics stats={stats} />
+      
+      
     </div>
   )
+
+ 
 
 };
 
